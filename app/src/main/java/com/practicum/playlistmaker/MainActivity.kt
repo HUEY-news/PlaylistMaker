39f1dca4cf1aa7f51b0e1d13f1db55dd1bf2ff46
Саид Исмаилов <HUEY.news@yandex.ru>
2023-08-31 21:10:57 +0300

    checkpoint: onClick methods implemented

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,43 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch: Button = findViewById(R.id.button_search)
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Нажата кнопка \"Поиск\"",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        val buttonLibrary: Button = findViewById(R.id.button_library)
+        buttonLibrary.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Нажата кнопка \"Медиатека\"",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val buttonSettings: Button = findViewById(R.id.button_settings)
+        buttonSettings.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Нажата кнопка \"Настройки\"",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }   
