e5554ede03455bb2cd526197fb565006cc305a39
Саид Исмаилов <HUEY.news@yandex.ru>
2023-09-02 22:10:25 +0300

    build: intent code realized

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,19 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val searchButton = findViewById<Button>(R.id.button_search)
-        searchButton.setOnClickListener{
-            // TODO: переход к экрану SearchActivity
+        searchButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SearchActivity::class.java))
         }
 
         val libraryButton = findViewById<Button>(R.id.button_library)
         libraryButton.setOnClickListener {
-            // TODO: переход к экрану LibraryActivity
+            startActivity(Intent(this@MainActivity, LibraryActivity::class.java))
         }
 
         val settingsButton: Button = findViewById(R.id.button_settings)
-        val settingsButtonClickListener : View.OnClickListener = object : View.OnClickListener {
+        // TODO: Переписать обработчик нажатий через лямбду...
+        val settingsButtonClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(view: View?) {
-                // TODO: переход к экрану SettingsActivity
+                startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
             }
         }
         settingsButton.setOnClickListener(settingsButtonClickListener)
