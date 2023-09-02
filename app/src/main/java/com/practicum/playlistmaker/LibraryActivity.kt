7775aa3478038260c8ec4a7d33cacb0acd854882
Саид Исмаилов <HUEY.news@yandex.ru>
2023-09-02 20:59:09 +0300

    build: homework task 3 (in progress...)

---
+++ app/src/main/java/com/practicum/playlistmaker/LibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.practicum.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -4,41 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonSearch: Button = findViewById(R.id.button_search)
-        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+        val searchButton = findViewById<Button>(R.id.button_search)
+        searchButton.setOnClickListener{
+            // TODO: переход к экрану SearchActivity
+        }
+
+        val libraryButton = findViewById<Button>(R.id.button_library)
+        libraryButton.setOnClickListener {
+            // TODO: переход к экрану LibraryActivity
+        }
+
+        val settingsButton: Button = findViewById(R.id.button_settings)
+        val settingsButtonClickListener : View.OnClickListener = object : View.OnClickListener {
             override fun onClick(view: View?) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Нажата кнопка \"Поиск\"",
-                    Toast.LENGTH_SHORT
-                ).show()
+                // TODO: переход к экрану SettingsActivity
             }
         }
-        buttonSearch.setOnClickListener(buttonSearchClickListener)
-
-        val buttonLibrary: Button = findViewById(R.id.button_library)
-        buttonLibrary.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Нажата кнопка \"Медиатека\"",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        val buttonSettings: Button = findViewById(R.id.button_settings)
-        buttonSettings.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Нажата кнопка \"Настройки\"",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
+        settingsButton.setOnClickListener(settingsButtonClickListener)
     }
 }   
