be36b402cb0186e2af79bfb418d567b7dfe1e869
Саид Исмаилов <HUEY.news@yandex.ru>
2023-09-03 17:14:22 +0300

    fix: settings layout remake

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,18 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageView>(R.id.button_back)
+        backButton.setOnClickListener{
+            startActivity(Intent(this@SettingsActivity, MainActivity::class.java))
+        }
     }
 }
