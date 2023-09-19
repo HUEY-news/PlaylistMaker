c916659e517a4e2edc880a8bd0011b384a8afbe9
Саид Исмаилов <HUEY.news@yandex.ru>
2023-09-19 18:53:52 +0300

    checkpoint

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,33 +18,31 @@ class SettingsActivity : AppCompatActivity() {
 
         val shareButton = findViewById<FrameLayout>(R.id.share_frame_layout)
         shareButton.setOnClickListener {
-            val text = "https://practicum.yandex.ru/android-developer"
-            val shareIntent = Intent(Intent.ACTION_SEND)
-            shareIntent.putExtra(Intent.EXTRA_TEXT, text)
-            startActivity(shareIntent)
+            val shareIntent = Intent(Intent.ACTION_SEND) // создаем новый Intent с действием ACTION_SEND
+            val shareIntentTitle = getString(R.string.share_app) // заголовок интента
+            shareIntent.type = "text/plain" // устанавливаем тип данных для отправки
+            val link = getString(R.string.yandex_practicum_android_developer_link) // создаём переменную для хранения ссылки на курс
+            shareIntent.putExtra(Intent.EXTRA_TEXT, link) // добавляем данные для отправки (ссылку на курс)
+            startActivity(Intent.createChooser(shareIntent, shareIntentTitle)) // запускаем Intent
         }
 
         val supportButton = findViewById<FrameLayout>(R.id.support_frame_layout)
         supportButton.setOnClickListener {
-            val email = "HUEY.news@yandex.ru"
-            val title = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
-            val text = "Спасибо разработчикам и разработчицам за крутое приложение!"
-            val supportIntent = Intent(Intent.ACTION_SENDTO)
-            supportIntent.data = Uri.parse("mailto:")
-            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-            supportIntent.putExtra(Intent.EXTRA_TITLE, title)
-            supportIntent.putExtra(Intent.EXTRA_TEXT, text)
-            startActivity(supportIntent)
+            val supportIntent = Intent(Intent.ACTION_SEND) // создаем новый Intent с действием ACTION_SEND
+            val supportIntentTitle = getString(R.string.write_to_user_support)
+            supportIntent.type = "message/rfc822" // устанавливаем тип данных для отправки
+            val email = getString(R.string.support_email) // email-адрес студента, выполняющего задание
+            val subject = getString(R.string.support_subject) // строка темы письма
+            val text = getString(R.string.support_text) // текст в теле письма
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email)) // добавляем данные об email-адресе студента
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject) // указываем тему письма
+            supportIntent.putExtra(Intent.EXTRA_TEXT, text) // добавляем текст письма для отправки
+            startActivity(Intent.createChooser(supportIntent, supportIntentTitle)) // запускаем Intent
         }
 
         val agreementButton = findViewById<FrameLayout>(R.id.agreement_frame_layout)
-        agreementButton.setOnClickListener {}
+        agreementButton.setOnClickListener {
+            // TODO: implicit intent
+        }
     }
-
-//    val message = "Привет, Android-разработка — это круто!"
-//    val shareIntent = Intent(Intent.ACTION_SENDTO)
-//    shareIntent.data = Uri.parse("mailto:")
-//    shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("yourEmail@ya.ru"))
-//    shareIntent.putExtra(Intent.EXTRA_TEXT, message)
-//    startActivity(shareIntent)
 }
