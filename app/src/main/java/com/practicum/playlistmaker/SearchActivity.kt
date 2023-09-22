17fa0f490ae14ecd6c425e94c00b7f3ede273c00
Саид Исмаилов <HUEY.news@yandex.ru>
2023-09-22 19:09:48 +0300

    build: search screen almost done

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,10 +2,17 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        // РЕАКЦИЯ ПРИ НАЖАТИИ НА КНОПКУ "НАЗАД"
+        val backButton = findViewById<ImageButton>(R.id.button_back)
+        backButton.setOnClickListener { finish() }
+
+
     }
 }
