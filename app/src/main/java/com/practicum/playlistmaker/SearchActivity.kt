8f7d71751720174e7c329c4602a05a42ea742d38
Саид Исмаилов <HUEY.news@yandex.ru>
2023-09-24 23:22:39 +0300

    Sprint 9 Homework: Some fixes after code review.

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -12,53 +12,36 @@ import android.widget.ImageButton
 
 class SearchActivity : AppCompatActivity()
 {
-    // БЛОК КЛЮЧЕВЫХ ЗНАЧЕНИЙ
-    companion object
-    {
-        // КЛЮЧ ДЛЯ ХРАНЕНИЯ СОДЕРЖИМОГО ПОИСКОВОЙ СТРОКИ
-        const val SEARCH_FIELD_CONTENT = "SEARCH_FIELD_TEXT"
-    }
-
-    // ГЛОБАЛЬНАЯ ПЕРЕМННАЯ ДЛЯ ХРАНЕНИЯ СОДЕРЖИМОГО ПОИСКОВОЙ СТРОКИ
-    lateinit var searchFieldContent : String
+    var searchFieldContent : String? = null
 
     override fun onCreate(state: Bundle?)
     {
         super.onCreate(state)
         setContentView(R.layout.activity_search)
 
-        // ВЫЗВАТЬ МЕТОД ONDESTROY ТЕКУЩЕЙ АКТИВИТИ
         val backButton = findViewById<ImageButton>(R.id.button_back)
         backButton.setOnClickListener { finish() }
 
         val searchField = findViewById<EditText>(R.id.search_field)
         val resetButton = findViewById<ImageButton>(R.id.button_reset)
         resetButton.setOnClickListener {
-            // ОЧИСТИТЬ ПОЛЕ ВВОДА
             searchField.setText("")
-            // ВСПОМОГАТЕЛЬНЫЙ КЛАСС. ПОСРЕДНИК МЕЖДУ ИСТОЧНИКОМ ВВОДА И ПРИЛОЖЕНИЕМ
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            // СКРЫТЬ КЛАВИАТУРУ
             inputMethodManager?.hideSoftInputFromWindow(searchField.windowToken, 0)}
 
-        // ИНТЕРФЕЙС СЛУШАТЕЛЯ ДЛЯ EDITTEXT
         val textWatcher = object : TextWatcher
         {
             override fun beforeTextChanged(string: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(string: Editable?) {}
             override fun onTextChanged(string: CharSequence?, start: Int, before: Int, count: Int)
             {
-                // МЕНЯЕМ ВИДИМОСТЬ КНОПКИ "CLEAR"
                 resetButton.visibility = clearButtonVisibility(string)
-
-                //СОХРАНЯМ ВВОД В ГЛОБАЛЬНУЮ ПЕРЕМЕННУЮ
                 if (string != null) searchFieldContent = string.toString()
             }
         }
         searchField.addTextChangedListener(textWatcher)
     }
 
-    // МЕТОД ДЛЯ РАБОТЫ С ПАРАМЕТРОМ VISIVILITY ЛЮБОГО НАСЛЕДНИКА VIEW
     fun clearButtonVisibility (string : CharSequence?) : Int
     {
         return if (string.isNullOrEmpty()) View.GONE else View.VISIBLE
@@ -67,14 +50,17 @@ class SearchActivity : AppCompatActivity()
     override fun onRestoreInstanceState(state: Bundle)
     {
         super.onRestoreInstanceState(state)
-        // ЗАГРУЖАЕМ СОДЕРЖИМОЕ ПОЛЯ ПОИСКА
         if (state != null) searchFieldContent = state.getString(SEARCH_FIELD_CONTENT, "")
     }
 
     override fun onSaveInstanceState(state: Bundle)
     {
         super.onSaveInstanceState(state)
-        // СОХРАНЯЕМ СОДЕРЖИМОЕ ПОЛЯ ПОИСКА
         state.putString(SEARCH_FIELD_CONTENT, searchFieldContent)
+    }
+
+    companion object
+    {
+        const val SEARCH_FIELD_CONTENT = "SEARCH_FIELD_TEXT"
     }
 }
