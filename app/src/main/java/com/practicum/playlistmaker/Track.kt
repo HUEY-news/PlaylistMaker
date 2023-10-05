e328c065e8af6952ff9da4300a6fd01e78860297
Саид Исмаилов <HUEY.news@yandex.ru>
2023-10-05 23:24:18 +0300

    Спринт 10/23 → Тема 6/6: День 3

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,8 +1,8 @@
 package com.practicum.playlistmaker
 
 class Track(
-    trackName: String, // НАЗВАНИЕ КОМПОЗИЦИИ
-    artistName: String, // ИМЯ ИСПОЛНИТЕЛЯ
-    trackTime: String, // ПРОДОЛЖИТЕЛЬНОСТЬ ТРЕКА
-    artWorkUrl100: String // ССЫЛКА НА ИЗОБРАЖЕНИЕ ОБЛОЖКИ
+    val trackName: String, // НАЗВАНИЕ КОМПОЗИЦИИ
+    val artistName: String, // ИМЯ ИСПОЛНИТЕЛЯ
+    val trackTime: String, // ПРОДОЛЖИТЕЛЬНОСТЬ ТРЕКА
+    val artWorkUrl100: String // ССЫЛКА НА ИЗОБРАЖЕНИЕ ОБЛОЖКИ
 )
