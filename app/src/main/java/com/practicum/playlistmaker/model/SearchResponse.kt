100dec592818b229b37e9bcacb384263f7b8bf35
Саид Исмаилов <HUEY.news@yandex.ru>
2023-10-20 21:37:40 +0300

    Спринт 11/23 ДЗ: День 1.

--- app/src/main/java/com/practicum/playlistmaker/model/SearchResponse.kt
+++ app/src/main/java/com/practicum/playlistmaker/model/SearchResponse.kt
@@ -1,6 +1,6 @@
 package com.practicum.playlistmaker.model
 
-class searchResponse(val resultCount: Int, val results: ArrayList<Result>) {
+class SearchResponse(val resultCount: Int, val results: ArrayList<Result>) {
     data class Result(
         val trackName: String,
         val artistName: String,
--- app/src/main/java/com/practicum/playlistmaker/network/iTunesAPI.kt
+++ app/src/main/java/com/practicum/playlistmaker/network/iTunesAPI.kt
@@ -1,6 +1,6 @@
 package com.practicum.playlistmaker.network
 
-import com.practicum.playlistmaker.model.searchResponse
+import com.practicum.playlistmaker.model.SearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,6 +10,6 @@ interface iTunesAPI {
     @GET("/search?entity=song")
     fun search(
         @Query("term") text: String
-    ): Call<searchResponse>
+    ): Call<SearchResponse>
 
 }
