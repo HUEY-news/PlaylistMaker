cf45f4d1698e5c558f03b6c4f08edd21f6ff8d5e
Саид Исмаилов <HUEY.news@yandex.ru>
2023-11-13 17:43:53 +0300

    1. Поле года сдвигается, если поля альбома нет. 2. Добавлены недостающие поля в модель трека.

--- app/src/main/java/com/practicum/playlistmaker/model/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/model/Track.kt
@@ -5,5 +5,9 @@ data class Track(
     val trackName: String, // НАЗВАНИЕ КОМПОЗИЦИИ
     val artistName: String, // ИМЯ ИСПОЛНИТЕЛЯ
     val trackTimeMillis: Int, // ПРОДОЛЖИТЕЛЬНОСТЬ ТРЕКА
-    val artworkUrl100: String // ССЫЛКА НА ИЗОБРАЖЕНИЕ ОБЛОЖКИ
+    val artworkUrl100: String, // ССЫЛКА НА ИЗОБРАЖЕНИЕ ОБЛОЖКИ
+    val collectionName: String, // НАЗВАНИЕ АЛЬБОМА
+    val releaseDate: String, // ГОД РЕЛИЗА ТРЕКА
+    val primaryGenreName: String, // ЖАНР ТРЕКА
+    val country: String // СТРАНА ИСПОЛНИТЕЛЯ
 )
