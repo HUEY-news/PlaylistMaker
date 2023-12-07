5affbe3c57034bd2e54ce3492dd117db3b485b9f
Саид Исмаилов <HUEY.news@yandex.ru>
2023-12-07 20:04:37 +0300

    1. Реализована логика debounce при клике на трек

---
+++ app/src/main/java/com/practicum/playlistmaker/Debounce.kt
@@ -0,0 +1,23 @@
+package com.practicum.playlistmaker
+
+import android.os.Handler
+import android.os.Looper
+
+class Debounce {
+    companion object{
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
+    }
+
+    private var isClickAllowed = true
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    fun clickDebounce() : Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({isClickAllowed = true}, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
+    }
+}
