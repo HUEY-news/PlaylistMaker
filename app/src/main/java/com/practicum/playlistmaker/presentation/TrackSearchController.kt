7fe0270dc9c5870505d58d168acab8d127a93d32
Саид Исмаилов <HUEY.news@yandex.ru>
2024-01-12 17:55:44 +0300

    16 Спринт: Присвоены id макетам, подключаемым через include. TrackSearchController избавлен от findViewById и полностью переведён на ViewBinding.

--- app/src/main/java/com/practicum/playlistmaker/presentation/TrackSearchController.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/TrackSearchController.kt
@@ -7,16 +7,10 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
-import androidx.recyclerview.widget.RecyclerView
 import com.practicum.playlistmaker.Creator
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.data.SearchHistory
@@ -31,20 +25,13 @@ class TrackSearchController (
     private val historyAdapter: TrackAdapter,
     private val searchHistory: SearchHistory
 ) {
-    private lateinit var binding: ActivitySearchBinding
+    private var _binding: ActivitySearchBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var emptyErrorText: String
     private lateinit var internetErrorText: String
     private var emptyErrorPlaceholder: Int = 0
     private var internetErrorPlaceholder: Int = 0
-
-    private lateinit var placeholderIcon: ImageView
-    private lateinit var placeholderText: TextView
-    private lateinit var placeholderButton: Button
-    private lateinit var searchHistoryContainer: LinearLayout
-    private lateinit var histotyRecycler: RecyclerView
-    private lateinit var searchHistoryButton: Button
-    private lateinit var progressBar: ProgressBar
 
     private val handler = Handler(Looper.getMainLooper())
     private val searchRunnable = Runnable { searchRequest(binding.searchField.text.toString()) }
@@ -52,7 +39,7 @@ class TrackSearchController (
 
 
     fun onCreate() {
-        binding = ActivitySearchBinding.inflate(activity.layoutInflater)
+        _binding = ActivitySearchBinding.inflate(activity.layoutInflater)
         activity.setContentView(binding.root)
 
         emptyErrorText = activity.resources.getString(R.string.placeholder_empty_error)
@@ -60,41 +47,33 @@ class TrackSearchController (
         emptyErrorPlaceholder = R.attr.placeholderEmptyError
         internetErrorPlaceholder = R.attr.placeholderInternetError
 
-        placeholderIcon = activity.findViewById(R.id.placeholderIcon)
-        placeholderText = activity.findViewById(R.id.placeholderText)
-        placeholderButton = activity.findViewById(R.id.placeholderButton)
-        searchHistoryContainer = activity.findViewById(R.id.searchHistoryContainer)
-        histotyRecycler = activity.findViewById(R.id.searchHistoryTrackList)
-        searchHistoryButton = activity.findViewById(R.id.searchHistoryButton)
-        progressBar = activity.findViewById(R.id.progressBar)
-
         activity.findViewById<ImageButton>(R.id.backButton).setOnClickListener { activity.finish() }
 
         binding.searchRecycler.adapter = searchAdapter
-        histotyRecycler.adapter = historyAdapter
+        binding.layoutSearchHistory.historyRecycler.adapter = historyAdapter
 
         if (searchHistory.getHistory().isNotEmpty()) {
             historyAdapter.setItems(searchHistory.getHistory())
-            searchHistoryContainer.isVisible = true
+            binding.layoutSearchHistory.searchHistoryContainer.isVisible = true
         }
 
         // реакция на нажатие кнопки "очистить историю":
-        searchHistoryButton.setOnClickListener {
-            searchHistoryContainer.isVisible = false
+        binding.layoutSearchHistory.searchHistoryButton.setOnClickListener {
+            binding.layoutSearchHistory.searchHistoryContainer.isVisible = false
             searchHistory.clearHistory()
         }
 
         // реализация отслеживания состояния фокуса поля поиска:
-        binding.searchField.setOnFocusChangeListener { view, hasFocus ->
+        binding.searchField.setOnFocusChangeListener { _, hasFocus ->
             if (searchHistory.getHistory().isNotEmpty()) {
                 historyAdapter.setItems(searchHistory.getHistory())
-                searchHistoryContainer.isVisible =
+                binding.layoutSearchHistory.searchHistoryContainer.isVisible =
                     hasFocus && binding.searchField.text.isEmpty()
             }
         }
 
         // реакция на нажатие кнопки "обновить":
-        placeholderButton.setOnClickListener {
+        binding.layoutPlaceholder.placeholderButton.setOnClickListener {
             searchRequest(binding.searchField.text.toString())
         }
 
@@ -112,10 +91,6 @@ class TrackSearchController (
             )
         }
 
-        fun searchDebounce() {
-            handler.removeCallbacks(searchRunnable)
-            handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
-        }
         // реакция на изменение текста в поле поиска:
         binding.searchField.addTextChangedListener(
             onTextChanged = { charSequence, _, _, _ ->
@@ -127,7 +102,7 @@ class TrackSearchController (
 
                 if (searchHistory.getHistory().isNotEmpty()) {
                     historyAdapter.setItems(searchHistory.getHistory())
-                    searchHistoryContainer.isVisible =
+                    binding.layoutSearchHistory.searchHistoryContainer.isVisible =
                         binding.searchField.hasFocus() && charSequence?.isEmpty() == true
                 }
             }
@@ -144,12 +119,12 @@ class TrackSearchController (
         if (request.isNotEmpty()) {
             hidePlaceholder()
             clearTrackList()
-            progressBar.isVisible = true
+            binding.progressBar.isVisible = true
 
             trackInteractor.searchTrack(request, object: TrackInteractor.TrackConsumer {
                 override fun consume(foundTrackList: List<Track>?) {
                     handler.post {
-                        progressBar.visibility = View.GONE
+                        binding.progressBar.visibility = View.GONE
                         if (foundTrackList != null) {
                             searchAdapter.setItems(foundTrackList)
                             binding.searchRecycler.isVisible = true
@@ -158,10 +133,9 @@ class TrackSearchController (
                     }
                 }
             })
-        } else {
-            clearTrackList()
-        }
+        } else { clearTrackList() }
     }
+
 
     private fun showPlaceholder(text: String) {
         clearTrackList()
@@ -171,14 +145,14 @@ class TrackSearchController (
         else if (text == internetErrorText) setInternetErrorState()
     }
     private fun setEmptyErrorState() {
-        placeholderIcon.setImageDrawable(getAttribute(emptyErrorPlaceholder))
-        placeholderText.text = emptyErrorText
-        placeholderButton.isVisible = false
+        binding.layoutPlaceholder.placeholderIcon.setImageDrawable(getAttribute(emptyErrorPlaceholder))
+        binding.layoutPlaceholder.placeholderText.text = emptyErrorText
+        binding.layoutPlaceholder.placeholderButton.isVisible = false
     }
     private fun setInternetErrorState() {
-        placeholderIcon.setImageDrawable(getAttribute(internetErrorPlaceholder))
-        placeholderText.text = internetErrorText
-        placeholderButton.isVisible = true
+        binding.layoutPlaceholder.placeholderIcon.setImageDrawable(getAttribute(internetErrorPlaceholder))
+        binding.layoutPlaceholder.placeholderText.text = internetErrorText
+        binding.layoutPlaceholder.placeholderButton.isVisible = true
     }
     private fun getAttribute(attr: Int): Drawable? {
         val attrs = intArrayOf(attr)
@@ -189,10 +163,11 @@ class TrackSearchController (
         return ContextCompat.getDrawable(activity, placeholderResourceId)
     }
 
+
     private fun hidePlaceholder() {
-        placeholderIcon.setImageDrawable(null)
-        placeholderText.text = null
-        placeholderButton.isVisible = false
+        binding.layoutPlaceholder.placeholderIcon.setImageDrawable(null)
+        binding.layoutPlaceholder.placeholderText.text = null
+        binding.layoutPlaceholder.placeholderButton.isVisible = false
     }
 
     private fun hideRecycler() {
@@ -203,6 +178,10 @@ class TrackSearchController (
         searchAdapter.setItems(arrayListOf())
     }
 
+    private fun searchDebounce() {
+        handler.removeCallbacks(searchRunnable)
+        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
+    }
     companion object {
         private const val SEARCH_DEBOUNCE_DELAY = 2000L
     }
