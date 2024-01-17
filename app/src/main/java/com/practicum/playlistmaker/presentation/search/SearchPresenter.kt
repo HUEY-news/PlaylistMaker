0e5a3161e6dba1daad386ac3379d3d1be3ac4798
Саид Исмаилов <HUEY.news@yandex.ru>
2024-01-17 23:58:16 +0300

    16 Спринт: MVP. Реализовано сохранение UI.

--- app/src/main/java/com/practicum/playlistmaker/presentation/search/SearchPresenter.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/search/SearchPresenter.kt
@@ -9,9 +9,24 @@ import com.practicum.playlistmaker.ui.search.model.SearchState
 import com.practicum.playlistmaker.util.Creator
 
 class SearchPresenter (context: Context) {
+
     private var view: SearchView? = null
-    fun attachView(view: SearchView){ this.view = view }
-    fun detachView() { this.view = null }
+    private var state: SearchState? = null
+    private var latestSearchText: String? = null
+
+    fun attachView(view: SearchView){
+        this.view = view
+        state?.let { view.render(it) }
+    }
+
+    fun detachView() {
+        this.view = null
+    }
+
+    private fun renderState(state: SearchState) {
+        this.state = state
+        this.view?.render(state)
+    }
 
     private val trackInteractor = Creator.provideTrackInteractor(context)
     private val handler = Handler(Looper.getMainLooper())
@@ -21,6 +36,8 @@ class SearchPresenter (context: Context) {
         searchRequest(newSearchText) }
 
     fun searchDebounce(changedText: String) {
+        if (latestSearchText == changedText) return
+        this.latestSearchText = changedText
         this.lastSearchText = changedText
         handler.removeCallbacks(searchRunnable)
         handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
@@ -28,18 +45,18 @@ class SearchPresenter (context: Context) {
 
     fun searchRequest(newSearchText: String) {
         if (newSearchText.isNotEmpty()) {
-            view?.render(SearchState.Loading)
+            renderState(SearchState.Loading)
 
             trackInteractor.searchTrack(newSearchText, object: TrackInteractor.TrackConsumer {
                 override fun consume(foundTrackList: List<Track>?, errorMessage: String?) {
                     handler.post {
                         view?.showProgressBar(false)
-                        if (foundTrackList != null) view?.render(SearchState.Content(foundTrackList))
-                        else if (errorMessage != null) view?.render(SearchState.Error(errorMessage))
+                        if (foundTrackList != null) renderState(SearchState.Content(foundTrackList))
+                        else if (errorMessage != null) renderState(SearchState.Error(errorMessage))
                     }
                 }
             })
-        } else { view?.render(SearchState.Content(listOf())) }
+        } else { renderState(SearchState.Content(listOf())) }
     }
 
     fun onDestroy() {
