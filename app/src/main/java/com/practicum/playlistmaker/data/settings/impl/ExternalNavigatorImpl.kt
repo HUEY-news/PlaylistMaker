c42f9e978b04c03fd650000b998692a9032a191c
Саид Исмаилов <HUEY.news@yandex.ru>
2024-01-22 17:55:07 +0300

    16 Спринт: MVVM. Исправлены баги при работе с экраном настроек.

--- app/src/main/java/com/practicum/playlistmaker/data/settings/impl/ExternalNavigatorImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/settings/impl/ExternalNavigatorImpl.kt
@@ -3,37 +3,43 @@ package com.practicum.playlistmaker.data.settings.impl
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.core.content.ContextCompat.startActivity
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.data.settings.api.ExternalNavigator
 
 class ExternalNavigatorImpl(private val context: Context): ExternalNavigator {
 
     override fun shareLink() {
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        val shareIntentTitle = context.getString(R.string.share_app)
-        shareIntent.type = "text/plain"
         val url = context.getString(R.string.YP_android_developer_link)
-        shareIntent.putExtra(Intent.EXTRA_TEXT, url)
-        context.startActivity(Intent.createChooser(shareIntent, shareIntentTitle))
+        val title = context.getString(R.string.share_app)
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, url)
+        val chooser = Intent.createChooser(intent, title)
+        chooser.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(context, chooser, null)
     }
 
     override fun openLink() {
-        val agreementIntent = Intent(Intent.ACTION_VIEW)
         val url = context.getString(R.string.agreement_link)
-        agreementIntent.data = Uri.parse(url)
-        context.startActivity(agreementIntent)
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(context, intent, null)
     }
 
     override fun openEmail() {
-        val supportIntent = Intent(Intent.ACTION_SEND)
-        val supportIntentTitle = context.getString(R.string.write_to_user_support)
-        supportIntent.type = "message/rfc822"
+        val title = context.getString(R.string.write_to_user_support)
         val email = context.getString(R.string.support_email)
         val subject = context.getString(R.string.support_subject)
         val text = context.getString(R.string.support_text)
-        supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-        supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
-        supportIntent.putExtra(Intent.EXTRA_TEXT, text)
-        context.startActivity(Intent.createChooser(supportIntent, supportIntentTitle))
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        val chooser = Intent.createChooser(intent, title)
+        chooser.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(context, chooser, null)
     }
 }
