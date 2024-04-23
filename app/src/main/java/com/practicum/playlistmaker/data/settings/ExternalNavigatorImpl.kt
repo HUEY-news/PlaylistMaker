93e6836624ef31de566bd3526f69ad8f89d1ae4f
Саид Исмаилов <HUEY.news@yandex.ru>
2024-04-23 13:56:46 +0300

    22 Спринт: Ревью. Исправлено некритичное замечание. Теперь при попытке написать в техподдержку почтовая программа открывается автоматически.

--- app/src/main/java/com/practicum/playlistmaker/data/settings/ExternalNavigatorImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/settings/ExternalNavigatorImpl.kt
@@ -28,15 +28,18 @@ class ExternalNavigatorImpl(private val context: Context): ExternalNavigator {
     }
 
     override fun openEmail() {
-        val title = context.getString(R.string.write_to_user_support)
+
         val email = context.getString(R.string.support_email)
+        val title = context.getString(R.string.write_to_user_support)
         val subject = context.getString(R.string.support_subject)
         val text = context.getString(R.string.support_text)
         val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "message/rfc822"
+
+        intent.data = Uri.parse("mailto:")
         intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
         intent.putExtra(Intent.EXTRA_SUBJECT, subject)
         intent.putExtra(Intent.EXTRA_TEXT, text)
+
         val chooser = Intent.createChooser(intent, title)
         chooser.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(context, chooser, null)
